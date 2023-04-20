fbea9a3fd2b187256fd0ed312fdc120aa93936f7
Boo <74915892+AndrewAshe-Student@users.noreply.github.com>
2023-04-20 23:16:53 +0100

    Update test_Project4.py

--- test_Project4.py
+++ test_Project4.py
@@ -5,14 +5,5 @@ import pytest
 #     output = Project4.getMakesByYear()
 #     assert output == "2020"
 
-# PyTest definitions and parameters
-@pytest.fixture
-def a2():
-    return 1
-
-@pytest.fixture
-def b2():
-    return 2
-
 def test_testForPytest():
-    Project4.testForPytest(a2, b2)
+    Project4.testForPytest(1, 2)
